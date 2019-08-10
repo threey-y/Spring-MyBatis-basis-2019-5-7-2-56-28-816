5b91b204d2ea67cab3eadcf48a71f138a11b6559
threey <threey.y@foxmail.com>
2019-08-10 17:33:17 +0800

    update

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,26 +1,63 @@
 package com.example.demo.entity;
 
+
+
 public class User {
+
     private long id;
+
     private String name;
 
 
+
+
+
     public long getId() {
+
         return id;
+
     }
 
+
+
     public void setId(long id) {
+
         this.id = id;
+
     }
+
+
+
 
 
     public String getName() {
+
         return name;
+
     }
+
+
 
     public void setName(String name) {
+
         this.name = name;
+
     }
 
-}
 
+
+    public User(int id,String name){
+
+        this.id = id;
+
+        this.name = name;
+
+    }
+
+
+
+    public User(){}
+
+
+
+}
